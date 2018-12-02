41a8f53e08566e7642f5459d707a4e3af9824e75
vivianafb <vivianafb@gmail.com>
2018-12-02 18:31:17 -0300

    Metodo create y product_params creados, junto a formulario en show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+
+    def create
+        @product = Product.new(product_params)
+        @product.save
+        redirect_to @product.category
+    end
+    
+    def product_params
+        params.require(:product).permit(:name, :price)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
+  resources :categories do
+    resources :products, only: [:create, :destroy]
+    end
   get 'categories/index'
   root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
