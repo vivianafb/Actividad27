09a06d57e5d3a82ad27517ed5e974432a6595086
vivianafb <vivianafb@gmail.com>
2018-12-02 17:45:51 -0300

    Migracion lista

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_204143) do
+ActiveRecord::Schema.define(version: 2018_12_02_204503) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
